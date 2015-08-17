47dcf9bd92149616ffbb7212f2b1dd8ac6f08036
Emmanuel Eduardo Cuevas Aparicio <emcuevas@mx1.ibm.com>
2015-08-16 23:06:00 -0500

    Initial Commit

---
+++ UIScrollView Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScrollView Solution
+//
+//  Created by Emmanuel Cuevas on 8/16/15.
+//  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScrollView Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollView Solution
+//
+//  Created by Emmanuel Cuevas on 8/16/15.
+//  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
